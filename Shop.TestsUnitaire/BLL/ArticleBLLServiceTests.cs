06c0f521741d9e84f12f0dc7ff9f3545c248a055
Pierre Bossis <pbossis@hotmail.com>
2024-05-17 18:45:29 +0200

    test unit 1

---
+++ Shop.TestsUnitaire/BLL/ArticleBLLServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using Moq;
+using Shop.BLL.Interfaces;
+using Shop.BLL.Services;
+using Shop.DAL.Entities;
+using Shop.DAL.Repositories;
+
+namespace Shop.TestsUnitaire.BLL
+{
+    public class ArticleBLLServiceTests
+    {
+        #region GetAll Tests
+        [Fact]
+        public void GetAllTest()
+        {
+            var mockArticleRepository = new Mock<IArticleRepository>();
+            List<ArticleEntity> list = [];
+
+            mockArticleRepository.Setup(repo => repo.GetAll()).Returns(list);
+
+            IArticleBLLRepository repository = new ArticleBLLService(mockArticleRepository.Object);
+
+            IEnumerable<ArticleEntity> entities = repository.GetAll();
+
+            Assert.NotNull(entities);
+            Assert.IsAssignableFrom<IEnumerable<ArticleEntity>>(entities);
+        }
+        #endregion
+
+        #region GetByReference Tests
+        [Fact]
+        public void GetByReferenceValidTest()
+        {
+            var mockArticleRepository = new Mock<IArticleRepository>();
+
+            mockArticleRepository.Setup(repo => repo.GetByReference(1)).Returns(new ArticleEntity() { Reference = 5});
+
+            IArticleBLLRepository repository = new ArticleBLLService(mockArticleRepository.Object);
+
+            ArticleEntity entity = repository.GetByReference(1);
+
+            Assert.NotNull(entity);
+            Assert.NotNull(entity.Reference);
+            Assert.IsType<ArticleEntity>(entity);
+        }
+
+        [Fact]
+        public void GetByReferenceInvalidTest()
+        {
+            var mockArticleRepository = new Mock<IArticleRepository>();
+
+            mockArticleRepository.Setup(repo => repo.GetByReference(It.IsAny<int>())).Returns((ArticleEntity)null);
+
+            IArticleBLLRepository repository = new ArticleBLLService(mockArticleRepository.Object);
+
+            ArticleEntity entity = repository.GetByReference(1);
+
+            Assert.Null(entity);
+        }
+        #endregion
+    }
+}
---
+++ Shop.TestsUnitaire/BLL/UserBLLServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using BCrypt.Net;
+using Moq;
+using Shop.BLL.Interfaces;
+using Shop.BLL.Services;
+using Shop.DAL.Entities;
+using Shop.DAL.Repositories;
+
+namespace Shop.TestsUnitaire.BLL
+{
+    public class UserBLLServiceTests
+    {
+        #region Login Tests
+        [Fact]
+        public void Login_Should_Return_Entity_When_Valid_Credentials()
+        {
+            //Arrange - préparation des data
+
+            // Crée un mock de IUserRepository pour simuler la couche de données
+            var mockUserRepository = new Mock<IUserRepository>();
+
+            // Configure le mock pour simuler une connexion réussie avec des identifiants valides
+            mockUserRepository.Setup(repo => repo.CheckPassword("validEmail")).Returns(BCrypt.Net.BCrypt.HashPassword("validPassword"));
+            mockUserRepository.Setup(repo => repo.Login("validEmail")).Returns(new UserEntity());
+
+            // Crée une instance de UserBLLService avec le mock de UserRepository
+            IUserBLLRepository repository = new UserBLLService(mockUserRepository.Object);
+
+            // Act - lance l'action
+
+            // Appelle la méthode Login avec des identifiants valides
+            UserEntity result = repository.Login("validEmail", "validPassword");
+
+            // Assert - test le résultat
+
+            // Vérifie que l'objet retourné n'est pas null et est du bon type
+            Assert.NotNull(result);
+            Assert.IsType<UserEntity>(result);
+
+        }
+
+        [Fact]
+        public void Login_Should_Return_Null_When_Invalid_Credentials()
+        {
+            //arrange
+            var mockUserRepository = new Mock<IUserRepository>();
+
+            mockUserRepository.Setup(repo => repo.CheckPassword("validEmail")).Returns(BCrypt.Net.BCrypt.HashPassword("validPassword"));
+            mockUserRepository.Setup(repo => repo.Login("validEmail")).Returns(new UserEntity());
+
+            IUserBLLRepository repository = new UserBLLService(mockUserRepository.Object);
+
+            //act
+            UserEntity result = repository.Login("invalidEmail", "invalidPassword");
+
+            //assert
+            Assert.Null(result);
+        }
+        #endregion
+
+        #region Register Tests
+
+        //[Fact]
+        //public void Register_Should_Return_Entity_When_Valid()
+        //{
+        //    // Arrange
+        //    var mockUserRepository = new Mock<IUserRepository>();
+
+        //    mockUserRepository.Setup(repo => repo.Register("test@gmail.com", "testNom", "testPrenom", It.IsAny<string>()))
+        //        .Returns(new UserEntity {DateCreation = DateTime.Now,Id = Guid.NewGuid(), IsAdmin = false, Email = "test@gmail.com", Nom = "testNom", Prenom = "testPrenom", MotDePasse = BCrypt.Net.BCrypt.HashPassword("test1234") });
+
+        //    IUserBLLRepository repository = new UserBLLService(mockUserRepository.Object);
+
+        //    // Act
+        //    UserEntity result = repository.Register("test@gmail.com", "testNom", "testPrenom", "test1234");
+
+        //    // Assert
+        //    Assert.NotNull(result);
+        //}
+
+        #endregion
+    }
+}
